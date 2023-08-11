25ca1ef26dce1e50968be78ab585d0be388f11a5
Manan Padia <35602706+PadiaManan@users.noreply.github.com>
2023-08-10 23:25:10 -0400

    calculator.py

--- calculator.py
+++ calculator.py
@@ -22,7 +22,7 @@ def divison(a, b):
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 result = divison(num1, num2)
-print("The sub is:", result)
+print("The division is:", result)
 
 def sub_numbers(a, b):
     return a * b
@@ -30,4 +30,12 @@ def sub_numbers(a, b):
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 result = sub_numbers(num1, num2)
-print("The mul is:", result)
+print("The multiplication is:", result)
+
+def mod_numbers(a, b):
+    return a % b
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+result = mod_numbers(num1, num2)
+print("The remainder is:", result)
