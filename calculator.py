8970aef6cfe9cb5d686ab7e5abf10230a36cdec2
Meet008 <meetchothani008@gmail.com>
2023-08-10 21:56:17 -0400

    changes added

--- calculator.py
+++ calculator.py
@@ -5,3 +5,12 @@ num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 result = add_numbers(num1, num2)
 print("The sum is:", result)
+
+
+def sub_numbers(a, b):
+    return a - b
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+result = sub_numbers_numbers(num1, num2)
+print("The sub is:", result)
